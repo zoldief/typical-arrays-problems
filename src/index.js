905d61fe9413dfdfebf6ac935a6b6efb47a2059d
Adolf <dgtronv@gmail.com>
2021-02-28 23:59:48 +0300

    feat: add typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (arguments.length === 0 || array.length === 0) {
+    return 0
+  } else {
+    let min = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
+      }
+    }
+    return min
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (arguments.length === 0 || array.length === 0) {
+    return 0
+  } else {
+    let max = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > max) {
+        max = array[i];
+      }
+    }
+    return max
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (arguments.length === 0 || array.length === 0) {
+    return 0
+  } else {
+    return array.reduce((a, b) => a + b) / array.length
+  }
 }
+
+
